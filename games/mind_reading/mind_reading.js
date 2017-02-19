21f56e7533556acc16ad1ceb213d2b18443a2799
hainguyenau <hainguyenau@gmail.com>
2017-02-19 14:16:45 -0800

    change effect when click for mind_reading

--- games/mind_reading/mind_reading.js
+++ games/mind_reading/mind_reading.js
@@ -7,9 +7,11 @@ $(document).ready(function(){
 	});
 
 	$('#magician').click(function(){
-		$('#cards').empty();
-		$('#cards').append("<h3>Your card has disappered!!! Don't belive me? Try to find it!!!</h3><br><img class='img-responsive' src='set2.jpg'/><br><img id='magician' class='img-responsive' src='magician.jpg'/>");
+		$(this).fadeOut('slow', function(){
+			$('#cards').empty();
+			$('#cards').append("<h3>Your card has disappered!!! Don't belive me? Try to find it!!!</h3><br><img class='img-responsive' src='set2.jpg'/><br><img id='magician' class='img-responsive' src='magician.jpg'/><br><div><a href='../../games.html'><button type='button' class='btn-lg btn-info'>Quit</button></a></div>");
+		});
+		
 	});
 	
-
 });
