e3039b7f3a62d6e151ce6be7e43a668826572079
hainguyenau <hainguyenau@gmail.com>
2017-02-23 16:55:14 -0800

    proposal game works

--- games/proposal/proposal.js
+++ games/proposal/proposal.js
@@ -2,16 +2,35 @@ $(document).ready(function(){
 	// Variables
 	var h = $('#myCanvas').height();
 	var w = $('#myCanvas').width();
-	// var x = Math.floor(Math.random()*w);
-	// var y = Math.floor(Math.random()*h);
+
 	
 	$('#no').click(function(){
-		$(this).animate({right:'-=5px'});
-	});
+		var x = (Math.random()-0.5)*(0.8*w);
+		var y = Math.random()*(0.8*h);
+		
+		
+		var next_left = x.toString()+ 'px';
+		var next_top = y.toString()+ 'px';
+		
 
+		console.log(next_left, next_top);
+		$(this).animate({left: next_left, top: next_top});
+
+	});
+	
+	$('#yes').click(function() {
+		$('#myCanvas').empty();
+		$('#myCanvas').fadeOut('slow');
+		$('#myCanvas').fadeIn('slow',function(){
+			$('#myCanvas').append("<h2>YOU HAVE MADE A RIGHT CHOICE!!! CONGRATULATIONS!!!</h2>");
+			
+		});
+		
+		
+		
+		
+	});
 });
 
-var rand_pos = function(){
-	
-};
+
 // Must add css position in order to animate left/right/up/down
