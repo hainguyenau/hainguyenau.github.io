2fa60f9dd3a1043cc61a3e8cc5c073f8280fe3dd
hainguyenau <hainguyenau@gmail.com>
2017-02-24 15:36:14 -0800

    complete proposal game

--- games/proposal/proposal.js
+++ games/proposal/proposal.js
@@ -1,37 +1,32 @@
 $(document).ready(function(){
 	// Variables
-	var h = $('#myCanvas').height();
-	var w = $('#myCanvas').width();
-
+	var h = $('#image').height();
+	var w = $('#image').width();
+	var button_h = $('#yes').height();
+	var button_w = $('#yes').width();
 	
+	// console.log(button_h, button_w);
 	$('#no').click(function(){
 		$('#whoosh')[0].play();
-		var x = (Math.random()-0.5)*(0.8*w);
-		var y = Math.random()*(0.8*h);
+		var x = Math.random()*(0.8*(w-button_w/2));
+		var y = Math.random()*(0.8*(h-button_h/2));
 		
-		
-		var next_left = x.toString()+ 'px';
 		var next_top = y.toString()+ 'px';
+		var next_left = x.toString()+ 'px';
 		
 
-		console.log(next_left, next_top);
+		// console.log(next_left, next_top);
 		$(this).animate({left: next_left, top: next_top});
-
 	});
 	
 	$('#yes').click(function() {
 		$('#bell')[0].play();
-		$('#myCanvas').empty();
-		$('#myCanvas').fadeOut('slow');
-		$('#myCanvas').fadeIn('slow',function(){
+		$('.wrapper').fadeOut('slow', function(){
 			$('#applause')[0].play();
-			$('#myCanvas').append("<img class='img-responsive' src='heart.png'/>");
-			
+			$('.wrapper').empty();
+			$('.wrapper').append("<img class='img-responsive'src='final_picture.jpg'/>");
+			$('.wrapper').fadeIn('slow');
 		});
-		
-		
-		
-		
 	});
 });
 
