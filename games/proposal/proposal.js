52a9da862c4199ec198b365aeca4ef686e6f916a
hainguyenau <hainguyenau@gmail.com>
2017-02-24 16:02:10 -0800

    fix proposal game when resize screen

--- games/proposal/proposal.js
+++ games/proposal/proposal.js
@@ -1,13 +1,12 @@
 $(document).ready(function(){
-	// Variables
-	var h = $('#image').height();
-	var w = $('#image').width();
-	var button_h = $('#yes').height();
-	var button_w = $('#yes').width();
-	
+
 	// console.log(button_h, button_w);
 	$('#no').click(function(){
 		$('#whoosh')[0].play();
+		var h = $('#image').height();
+		var w = $('#image').width();
+		var button_h = $('#yes').height();
+		var button_w = $('#yes').width();
 		var x = Math.random()*(0.8*(w-button_w/2));
 		var y = Math.random()*(0.8*(h-button_h/2));
 		
