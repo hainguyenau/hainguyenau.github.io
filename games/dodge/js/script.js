50bfff55fccade1e5b664d61d7e0e83f669f9e44
hainguyenau <hainguyenau@gmail.com>
2017-04-16 18:51:12 -0700

    game: dodge add more enemies

--- games/dodge/js/script.js
+++ games/dodge/js/script.js
@@ -8,22 +8,18 @@ var r_move_x = 10,
 function init() {
 	var canvas = document.getElementById("my_canvas");
 	var stage = new createjs.Stage(canvas);
-	var SIZE = 100
+	var SIZE = 50;
 	
 
-	// Create enemy
+	// Create list of enemies and friends
+	var enemies = [["ball", "burger", "fries"],["veggie", "banana", "apple"]];
 	
 	// var r = new createjs.Shape();
 	// r.graphics.beginFill("blue").rect(0, 0, SIZE, SIZE);
 	
-	var r = new createjs.Bitmap("assets/ball.png");
-	r.regX = SIZE/2;
-	r.regY = SIZE/2;
+
 	
-	r.x = 1;
-	r.y = 400;
-	
-	// Create rectangle player
+	// Create player
 	var me = new createjs.Shape();
 	me.graphics.beginFill("red").rect(0, 0, SIZE, SIZE);
 	me.regX = SIZE/2;
@@ -35,16 +31,18 @@ function init() {
 	// Keyboard handler
 	window.onkeydown = keyDownHandler;
 	
-	// Add children
+	// Add player
 	stage.addChild(me);
-	stage.addChild(r);
 	
+	// Add random initial enemy/friend
+	r = createEnemy();
+	stage.addChild(r);
 	// Ticker
 	createjs.Ticker.setFPS(30);
 	createjs.Ticker.addEventListener("tick", handleTick);
 	
 	function handleTick(event) {
-		r.rotation += 10;
+		r.rotation += 5;
 		r.x += r_move_x;
 		r.y += r_move_y;
 		
@@ -73,23 +71,30 @@ function init() {
 			r.y = SIZE/2;
 			r_move_y *= -1;
 		} 
-
 		
 		// Collision with red square 
 		if (Math.abs(r.x - me.x) < SIZE && Math.abs(r.y - me.y) < SIZE) {
 			stage.removeChild(me);
+			stage.removeChild(r);
+			
 			setTimeout(function(){
 				stage.addChild(me);
+			}, 2000);
+			
+			setTimeout(function(){
+				r = createEnemy();
+				stage.addChild(r);
 			}, 3000);
 		}
 		
 		// Make me bigger if hit
 		
 		
-		// Make blue rect faster over time
+		// Make enemy rect faster over time
 		
 		
 		// Animate me getting scared when the square is near, but not hit
+		
 		stage.update();
 		
 
@@ -112,7 +117,18 @@ function init() {
 		}
 	}
 
-	
+	function createEnemy(){
+		var index_list = Math.floor(Math.random()*2);
+		var index_item = Math.floor(Math.random()*3);
+		var link = "assets/" + enemies[index_list][index_item] + ".png";
+		var r = new createjs.Bitmap(link);
+		r.regX = SIZE/2;
+		r.regY = SIZE/2;
+		r.x = 1;
+		r.y = 400;
+		return r;
+		
+	}
 	
 	
 }
