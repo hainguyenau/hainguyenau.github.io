1bb3f85674265b653acfc72161ac619e7b29c7d7
hainguyenau <hainguyenau@gmail.com>
2017-05-04 16:11:23 -0700

    license game: add more graphics

--- games/license_plate/js/script.js
+++ games/license_plate/js/script.js
@@ -1,6 +1,9 @@
 var license;
 var score = 0;
 var time = 5000;
+var rectWidth = 60;
+var rectHeight = 10;
+var level = 1;
 function init() {
 	var canvas = document.getElementById("my_canvas");
 	var stage = new createjs.Stage(canvas);
@@ -12,13 +15,30 @@ function init() {
 	stage.addChild(ScoreText);
 	stage.update();
 	
+	// show level
+	var LevelText = new createjs.Text("Level: " + level, "bold 20px Times", "white");
+	LevelText.x = 420;
+	LevelText.y = 10;
+	stage.addChild(LevelText);
+	stage.update()
+	
 	// Create license plate number
 	license = createLicense();
-	var licenseGraphic = new createjs.Text(first + letters + nums, "bold 40px Times", "black");
+	var licenseGraphic = new createjs.Text(first + letters + nums, "bold 30px Times", "black");
+	licenseGraphic.shadow = new createjs.Shadow('#000000', 0.2, 0.7, 2);
+	var plate = new createjs.Shape();
+	plate.graphics.beginStroke("black");
+	plate.graphics.beginFill("white").drawRoundRect(-15, -10, 150, rectWidth, rectHeight);
+
+	
 	setTimeout(function(){
+		stage.addChild(plate);
 		stage.addChild(licenseGraphic);
-		licenseGraphic.x = Math.random()*(canvas.width-150);
-		licenseGraphic.y = Math.random()*(canvas.height-100);
+		
+		licenseGraphic.x = Math.random()*(canvas.width-100-rectWidth);
+		licenseGraphic.y = Math.random()*(canvas.height-100-rectHeight);
+		plate.x = licenseGraphic.x;
+		plate.y = licenseGraphic.y;
 		stage.update();
 	},2000);
 	
@@ -26,13 +46,15 @@ function init() {
 	// Remove license plate 2 seconds after license is created
 	setTimeout(function(){
 		stage.removeChild(licenseGraphic);
+		stage.removeChild(plate);
 		stage.update();
 	}, 2000+time);
 	
 	// Shorten flashing speed if score is a multiple of 10
 	if (score%10==0 && score!=0){
 		time = 0.7*time;
-		alert('Level up. Speed increases.')
+		alert('Level up. Speed increases.');
+		level += 1;
 	}
 
 }
