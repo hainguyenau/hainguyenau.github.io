1c716f405ae43eb9046a41de391c4f8bf454e022
hainguyenau <hainguyenau@gmail.com>
2017-05-06 01:40:38 -0700

    license game: fix animations

--- games/license_plate/js/script.js
+++ games/license_plate/js/script.js
@@ -1,12 +1,13 @@
 var license;
 var score = 0;
 var time = 4000;
-var rectWidth = 60;
-var rectHeight = 10;
+var rectWidth = 100;
+var rectHeight = 40;
 var level = 1;
 function init() {
 	var canvas = document.getElementById("my_canvas");
 	var stage = new createjs.Stage(canvas);
+	var result = document.getElementById("result");
 	
 	// show score
 	var ScoreText = new createjs.Text("Score: "+ score, "bold 20px Times", "white");
@@ -14,6 +15,8 @@ function init() {
 	ScoreText.y = 10;
 	stage.addChild(ScoreText);
 	stage.update();
+	// Game Over
+
 	
 	// show level
 	var LevelText = new createjs.Text("Level: " + level, "bold 20px Times", "white");
@@ -24,22 +27,33 @@ function init() {
 	
 	// Create license plate number
 	license = createLicense();
-	var licenseGraphic = new createjs.Text(first + letters + nums, "bold 30px Times", "black");
-	licenseGraphic.shadow = new createjs.Shadow('#000000', 0.2, 0.7, 2);
+	var licenseGraphic = new createjs.Text(first + letters + nums, "bold 20px Times", "black");
+	licenseGraphic.shadow = new createjs.Shadow('#878787', 0.2, 0.7, 2);
+	licenseGraphic.set({
+		textAlign: "center",
+		textBaseline: "middle",
+		x: rectWidth/2,
+		y: rectHeight/2
+	})
+
+	// Create plate
 	var plate = new createjs.Shape();
 	plate.graphics.beginStroke("black");
-	plate.graphics.beginFill("white").drawRoundRect(-15, -10, 150, rectWidth, rectHeight);
+	plate.graphics.beginFill("white").drawRoundRect(0, 0, rectWidth, rectHeight, 7);
+	plate.regX = rectWidth/2;
+	plate.regY = rectHeight/2;
 
 	
 	setTimeout(function(){
 		stage.addChild(plate);
 		stage.addChild(licenseGraphic);
 		
-		licenseGraphic.x = Math.random()*(canvas.width-100-rectWidth);
-		licenseGraphic.y = Math.random()*(canvas.height-100-rectHeight);
+		licenseGraphic.x = Math.random()*(canvas.width-rectWidth) + rectWidth/2;
+		licenseGraphic.y = Math.random()*(canvas.height-rectHeight) + rectHeight/2;
 		plate.x = licenseGraphic.x;
 		plate.y = licenseGraphic.y;
 		stage.update();
+		console.log(plate.x, plate.y);
 	},3000);
 	
 
@@ -50,11 +64,23 @@ function init() {
 		stage.update();
 	}, 3000+time);
 	
-	// Shorten flashing speed if score is a multiple of 10
-	if (score%10==0 && score!=0){
-		time = 0.7*time;
-		alert('Level up. Speed increases.');
+	// Next level
+	if (score>=10){
+		// Display notification
+		var LevelUp = document.createElement('img');
+		LevelUp.setAttribute('src','img/level_up.png');
+		LevelUp.setAttribute('width', '70px');
+		result.appendChild(LevelUp);
+		result.classList.add('fadeOutUp');
 		level += 1;
+		stage.update();
+		time = 0.7*time;
+		
+		// Remove animation class to add next time
+		setTimeout(function(){
+			result.removeChild(result.lastChild);
+			result.classList.remove('fadeOutUp');
+	},3000)
 	}
 
 }
@@ -69,7 +95,6 @@ function createLicense() {
 		tempNum = (Math.floor(Math.random()*9)).toString();
 		letters += tempLetter;
 		nums += tempNum;
-		console.log(i, tempLetter, tempNum);
 	}
 	return first + letters + nums;
 
@@ -77,21 +102,20 @@ function createLicense() {
 
 function show_result(){
 	var answer = document.getElementById("input").value;
-	var result = document.getElementById("result");
 	if (answer.toUpperCase() === license) {
 		var yes = document.createElement('img');
 		yes.setAttribute('src','img/yes.png');
-		yes.setAttribute('width', '80px');
+		yes.setAttribute('width', '70px');
 		result.appendChild(yes);
 		result.classList.add('tada');
-		score = score + 2;
+		score += 2;
 	} else {
 		var no = document.createElement('img');
 		no.setAttribute('src','img/no.png');
-		no.setAttribute('width', '80px');
+		no.setAttribute('width', '70px');
 		result.appendChild(no);
 		result.classList.add('tada');
-		score --;
+		score -= 2;
 	}
 	// Empty input form
 	document.getElementById("input").value = "";
